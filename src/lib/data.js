de300bedfebb01dd6b7ba6b3f3d8dad3973df715
Leslie-L <leslie.centeno@galileo.edu>
2024-04-15 19:01:46 -0600

    categories ready

---
+++ src/lib/data.js
@@ -0,0 +1,93 @@
+export const moviesWords = [
+    { id: "movie_001", word: "action" },
+    { id: "movie_002", word: "adventure" },
+    { id: "movie_003", word: "animation" },
+    { id: "movie_004", word: "comedy" },
+    { id: "movie_005", word: "crime" },
+    { id: "movie_006", word: "drama" },
+    { id: "movie_007", word: "fantasy" },
+    { id: "movie_008", word: "horror" },
+    { id: "movie_009", word: "mystery" },
+    { id: "movie_010", word: "romance" },
+    { id: "movie_011", word: "movie" },
+    { id: "movie_012", word: "thriller" },
+  ];
+  
+export const tvShowsWords = [
+    { id: "tv_001", word: "detective" },
+    { id: "tv_002", word: "cooking" },
+    { id: "tv_003", word: "rravel" },
+    { id: "tv_004", word: "makeover" },
+    { id: "tv_005", word: "reality" },
+    { id: "tv_006", word: "competition" },
+    { id: "tv_007", word: "television" },
+    { id: "tv_008", word: "news" },
+    { id: "tv_009", word: "documentary" },
+    { id: "tv_010", word: "soap Opera" },
+    { id: "tv_011", word: "children's" },
+    { id: "tv_012", word: "music" },
+    
+  ];
+  export const countries = [
+    { id: "country_gt_001", word: "Guatemala" },
+    { id: "country_ng_002", word: "Nigeria" },
+    { id: "country_can_003", word: "Canada" },
+    { id: "country_aus_004", word: "Australia" },
+    { id: "country_ger_005", word: "Germany" },
+    { id: "country_fra_006", word: "France" },
+    { id: "country_jpn_007", word: "Japan" },
+    { id: "country_ita_008", word: "Italy" },
+    { id: "country_spa_009", word: "Spain" },
+    { id: "country_chn_010", word: "China" },
+    { id: "country_ind_011", word: "India" },
+    { id: "country_bra_012", word: "Brazil" },
+   
+  ];
+  
+  export const capitalCities = [
+    { id: "capital_001", word: "Washington" },
+    { id: "capital_002", word: "London" },
+    { id: "capital_003", word: "Ottawa" },
+    { id: "capital_004", word: "Canberra" },
+    { id: "capital_005", word: "Berlin" },
+    { id: "capital_006", word: "Paris" },
+    { id: "capital_007", word: "Tokyo" },
+    { id: "capital_008", word: "Rome" },
+    { id: "capital_009", word: "Madrid" },
+    { id: "capital_010", word: "Beijing" },
+    { id: "capital_011", word: "NewDelhi" },
+    { id: "capital_012", word: "Brasilia" },
+  ];
+  
+ export  const animals = [
+    { id: "animal_001", word: "Lion" },
+    { id: "animal_002", word: "Elephant" },
+    { id: "animal_003", word: "Tiger" },
+    { id: "animal_004", word: "Giraffe" },
+    { id: "animal_005", word: "Zebra" },
+    { id: "animal_006", word: "Monkey" },
+    { id: "animal_007", word: "Panda" },
+    { id: "animal_008", word: "Kangaroo" },
+    { id: "animal_009", word: "Penguin" },
+    { id: "animal_010", word: "Dolphin" },
+    { id: "animal_011", word: "Squirrel" },
+    { id: "animal_012", word: "Horse" },
+    
+  ];
+  
+  export const sports = [
+    { id: "sport_001", word: "Football" },
+    { id: "sport_002", word: "Basketball" },
+    { id: "sport_003", word: "Tennis" },
+    { id: "sport_004", word: "Soccer" },
+    { id: "sport_005", word: "Baseball" },
+    { id: "sport_006", word: "Golf" },
+    { id: "sport_007", word: "Rugby" },
+    { id: "sport_008", word: "Cricket" },
+    { id: "sport_009", word: "Swimming" },
+    { id: "sport_010", word: "Boxing" },
+    { id: "sport_011", word: "Volleyball" },
+    { id: "sport_012", word: "Hockey" },
+    
+  ];
+  
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,6 +1,48 @@
+import { useEffect, useState } from "react";
+import { useNavigate} from "react-router-dom";
+
+import ButtonGeneral from "../components/ButtonGeneral";
+
+
 function Category() {
-    return(
-        <h1>Category</h1>
-    )
+    const [categories, setCategories] = useState([])
+    const navigation = useNavigate()
+    useEffect(()=>{
+        
+         fetch('./lib/categories.json')
+            .then(data=>data.json())
+            .then(res=>setCategories(res))
+        
+    },[])
+    const handleCategory =(name)=>{
+        const newRoute = `/game/${name}`;
+        navigation(newRoute)
+    }
+    const handleBack =()=>{
+        
+        navigation('/')
+    }
+    
+  return (
+    <section className="w-72 h-96 rounded-lg px-3 py-1 bg-blueModal">
+      <div className="w-full h-full flex flex-col justify-center items-center gap-4">
+        <div className="w-full flex justify-between">
+            <button onClick={handleBack} className="h-8 w-8 rounded-full bg-rose-500 border-gray-800 border-2 shadow-lg  text-white grid place-content-center hover:bg-white hover:text-rose-500">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M12 9.059V6.5a1.001 1.001 0 0 0-1.707-.708L4 12l6.293 6.207a.997.997 0 0 0 1.414 0A.999.999 0 0 0 12 17.5v-2.489c2.75.068 5.755.566 8 3.989v-1c0-4.633-3.5-8.443-8-8.941" />
+                </svg>
+            </button>
+            <h1 className="font-bold text-xl text-center text-white">
+                Pick a Category
+            </h1>
+        </div>
+            {
+                categories.map(({id,category})=>{
+                    return <ButtonGeneral key={id} text={category} click={()=>{handleCategory(id)}}/>
+                })
+            }
+      </div>
+    </section>
+  );
 }
 export default Category;
